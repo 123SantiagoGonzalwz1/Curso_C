8b64936dc18fc809630036e200a245ab2b50688e
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2024-06-02 20:23:47 -0500

    Primera clase de c#

---
+++ first_c.cs
@@ -0,0 +1,13 @@
+// Console.WriteLine("Hello World!");
+
+Console.Write("Congratulations!");
+Console.Write(" ");
+
+// "Console es la clase", ". es la navegación de la clase al método", "WriteLine es el método" y "; determina que se finalizao el comando"
+Console.WriteLine("Congratulations!");
+Console.Write("You wrote your first lines of code.");
+
+Console.WriteLine("This is the first Line.");
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
