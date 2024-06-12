ff85f86fe82d98269f0d57e1e5fa88e2cba2d5c1
Santiago González <109437318+123SantiagoGonzalwz1@users.noreply.github.com>
2024-06-12 08:26:04 -0500

    Segunda clase de c#

---
+++ declaracion_de_variables.cs
@@ -0,0 +1,6 @@
+// Declarar variable
+string firstName; // Usa Camel Case una letra comienza en minúscula y la de la siguiente palabra en Mayúscula
+char userOption;
+int gameScore;
+decimal particlesPerMillion;
+bool processedCustomer;
---
+++ ejercicio1.cs
@@ -0,0 +1,24 @@
+// Los tipos de datos deben coincidir con el asignado
+// Recuperar un valor de una variable 
+string firstName; // Declarar
+firstName = "Bob"; // Asignar
+string lastname = "Bob Esponja"; // Inicializar variable
+Console.WriteLine(lastname);
+Console.WriteLine(firstName);
+
+// Reasignación del valor de una variable
+string secondName;
+secondName = "Bob";
+Console.WriteLine(secondName);
+secondName = "Liem";
+Console.WriteLine(secondName);
+secondName = "Isabella";
+Console.WriteLine(secondName);
+secondName = "Yasmin";
+Console.WriteLine(secondName);
+
+// Varibales locales de tipo implícito
+var message = "Hello World"; // Se usa la palabre clave "var"
+
+
+
