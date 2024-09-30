58036270ac71d71f33fc419d72660d5416f08fd1
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2024-09-30 12:29:29 -0500

    Creando más programas

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -40,7 +40,7 @@ else
     Console.WriteLine("You need a coat!");
 } */
 
-// Crear un programa que verifique si un número es positivo o negativo.
+/* // Crear un programa que verifique si un número es positivo o negativo.
 Console.WriteLine("Por  favor, ingrese un número: ");
 int inputNumber = Convert.ToInt32(Console.ReadLine());
 
@@ -52,5 +52,35 @@ if(inputNumber > 0) {
 
 else {
     Console.WriteLine("El número es negativo");
+}
+
+// Escribir un programa que tome dos números de la consola y muestre el máximo de los dos.
+Console.WriteLine("Ingrese un número, por favor: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese un número, por favor: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 > number2) {
+    Console.WriteLine("El primer número es mayor");
+}
+
+else {
+    Console.WriteLine("El segundo número es mayor");
+} */
+
+// Escribir un programa para clasificar una edad determinada.
+Console.WriteLine("Ingrese su edad: ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+// Categorizar la edad como "niño" (menos de 12 años), "adolescente" (de 12 a 18 años),
+// "adulto" (de 19 a 60 años) o "mayor" (más de 60 años)
+if (age < 12) {
+    Console.WriteLine("Eres un niño");
+} else if (age >= 12 && age <= 18) {
+    Console.WriteLine("Eres un adolescente");
+} else if (age >= 19 && age <= 60) {
+    Console.WriteLine("Eres un adulto");
+} else {
+    Console.WriteLine("Eres un adulto mayor");
 }
 
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff85f86fe82d98269f0d57e1e5fa88e2cba2d5c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae5e49028e72e64aa94a3703816de0d7abbf1566")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
