bbd8b3e3ec4da82d86cb3d2327ea9b1ac8275a43
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2024-10-14 19:53:56 -0500

    Ciclos con foreach

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d59d9b8295694786b6481fdb43e4ec27077516a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e9126b00335b8c1ea04e4e76652970d5e7286e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -109,5 +109,131 @@ if (discountPercentage > 0)
 
 } */
 
+/* string[] fraudulentOrderIDs = new string[3];
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789"; */
+
+/* string[]  fraudulentOrderIDs = [ "A123", "B456", "C789" ];
 
 
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process."); */
+/* fraudulentOrderIDs[0] = "F000";
+Console.WriteLine($"Reassing First:  {fraudulentOrderIDs[0]}"); */
+
+/* string[] names = { "Rowena", "Robin", "Bao" };
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+} */
+
+/* int[] inventory = { 200, 450, 700, 175, 250};
+int sum = 0;
+int bin = 0;
+
+foreach (int items in inventory)
+{
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+
+Console.WriteLine($"We have {sum} items in inventory."); */
+
+// Identificar pedidos fraudulentos
+/* string[] fraudulentOrderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string orders in fraudulentOrderIDs)
+{
+    // Informe de los identificadores que comienzan con la letra 'B'
+    if (orders.StartsWith("B")) {
+        Console.WriteLine(orders);
+    }
+} */
+
+// Nombres de variables locales
+/* char userOption;
+int gameScore;
+float particlesPerMillion;
+bool processedCustomer; */
+
+// This is a code comment!
+
+/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+*/
+/* Random random = new Random();
+string[] orderIDs = new string[5];
+
+for (int i = 0; i < orderIDs.Length; i++)
+{
+    int prefixValue = random.Next(65, 70);
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
+
+    orderIDs[i] = prefix + suffix;
+}
+
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+} */
+
+// Código con buenas legibilidad
+/* Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+} */
+
+// Desafio mejorar la legibilidad del código
+
+/*
+    El proposito de este código es invertir una cadena
+    y contar el número de veces que aparece un cáracter
+    determinado.
+*/
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] message = originalMessage.ToCharArray();
+Array.Reverse(message);
+
+int letterCount = 0;
+
+foreach (char letter in message)
+{
+    if (letter == 'o')
+    {
+        letterCount++;
+    }
+}
+
+string newMessage = new String(message);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
