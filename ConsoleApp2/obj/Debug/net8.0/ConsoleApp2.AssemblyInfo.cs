b4e170a8be7ac0d598823f0cbc9c7ffe400d08f0
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2024-10-22 18:23:25 -0500

    Distintos tipos de conversiones y cómo usarlos

--- ConsoleApp2/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
+++ ConsoleApp2/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae5ce824921bcbbeb4b644e0389945e036436f8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b711b130fa7c226c2d3e5d6af68e544394e8eb40")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
