f62670f062eb04f4677cdd533684702cec2eb16c
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2024-11-04 17:58:11 -0500

    Creando métodos con parametros en la firma del método

--- Metodos/obj/Debug/net8.0/Metodos.AssemblyInfo.cs
+++ Metodos/obj/Debug/net8.0/Metodos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Metodos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ca400ccf853176597484e234f713a4d1802502d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d22f7f4be389bb9a5b82b021e73254d3e7272a86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Metodos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Metodos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
