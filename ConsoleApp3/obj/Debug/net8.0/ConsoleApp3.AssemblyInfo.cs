9e8f93c020c5c3cb18ec2c2d5f0b02f4e514b840
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2024-11-18 18:36:40 -0500

    Haciendo varios ejercicios de depuración

--- ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
+++ ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df34409bf9c90b6ccbf6ab2e2ff22ece5d642d85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83d2445213a3df01591b5c216e804728f833dfe8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
