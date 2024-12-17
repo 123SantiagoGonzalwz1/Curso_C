cf6457d40736f2fa98f5a20b7eb83bc90bd29854
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2024-12-17 12:13:06 -0500

    Trabajando con archivos y directorios

---
+++ SistemaDeArchivos/Program.cs
@@ -0,0 +1,20 @@
+﻿IEnumerable<string> listOfDirectories = Directory.EnumerateDirectories("stores");
+
+foreach (var dir in listOfDirectories)
+{
+    Console.WriteLine(dir);
+}
+
+IEnumerable<string> files = Directory.EnumerateFiles("stores");
+
+foreach (var file in files)
+{
+    Console.WriteLine(file);
+}
+
+IEnumerable<string> allFilesInAllFolders = Directory.EnumerateFiles("stores", "*.txt", SearchOption.AllDirectories);
+
+foreach (var file in allFilesInAllFolders)
+{
+    Console.WriteLine(file);
+}
